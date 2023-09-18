6423be1a3b6b81ec68f900ea47d187201c325f9a
Gilang Fatahilah Akbar <gilangf010@gmail.com>
2023-09-18 15:45:29 +0700

    upload main files

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
 };
+
+const signs = document.querySelectorAll("x-sign");
+const randomIn = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+
+const mixupInterval = (el) => {
+  const ms = randomIn(2000, 4000);
+  el.style.setProperty("--interval", `${ms}ms`);
+};
+
+signs.forEach((el) => {
+  mixupInterval(el);
+  el.addEventListener("webkitAnimationIteration", () => {
+    mixupInterval(el);
+  });
+});
